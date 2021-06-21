276113a1489cb9db950a47fa1d5e08cf92a51707
Yolanda Ornellas <ymf.ornellas@icloud.com>
2021-06-21 11:49:24 -0300

    Create Project

---
+++ src/main/java/com/github/yornellas/projectorder/ProjectOrderApplication.java
@@ -0,0 +1,13 @@
+package com.github.yornellas.projectorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/yornellas/projectorder/ProjectOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.yornellas.projectorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
