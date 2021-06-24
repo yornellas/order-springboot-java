0d21c54049bc14b4cb9708da2ea35da89195d2e0
Yolanda Ornellas <ymf.ornellas@icloud.com>
2021-06-24 14:10:45 -0300

    Exception handling - findById

--- src/main/java/com/github/yornellas/projectorder/services/UserService.java
+++ src/main/java/com/github/yornellas/projectorder/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.github.yornellas.projectorder.entities.User;
 import com.github.yornellas.projectorder.repositories.UserRepository;
+import com.github.yornellas.projectorder.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,7 +23,7 @@ public class UserService {
 	public User findById(Long id) {
 		Optional<User> object = repository.findById(id);
 		
-		return object.get();
+		return object.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
